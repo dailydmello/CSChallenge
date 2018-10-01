acd2ba679a24a9a7c0c285cf061f8dca68d8935c
dailydmello <dmelloe@mcmaster.ca>
2018-09-30 22:15:55 -0400

    Refactored Code

--- CSChallenge/CSChallenge/Controllers/PostTableViewController.swift
+++ CSChallenge/CSChallenge/Controllers/PostTableViewController.swift
@@ -11,6 +11,7 @@ import Alamofire
 import SwiftyJSON
 
 class PostTableViewController: UITableViewController {
+    
     var posts = [Post](){
         didSet {
             tableView.reloadData()
@@ -20,33 +21,7 @@ class PostTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let apiToContact = "https://www.reddit.com/.json"
-        Alamofire.request(apiToContact).validate().responseJSON() { response in
-            switch response.result{
-            case .success:
-                if let value = response.result.value {
-                    
-                    let json = JSON(value)
-                    
-                    let json_children = json["data"]["children"].array
-                    print(json)
-                    
-                    if let json_children = json_children{
-                        self.posts = json_children.flatMap({
-                            Post.init(json:$0)
-                        })
-                        print(self.posts)
-                        print(self.posts.count)
-                        
-                        
-                    }
-                }
-                
-            case .failure(let error):
-                print("API request did not succeed")
-                print(error)
-            }
-        }
+        retrieveAndParseData()
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -57,19 +32,37 @@ class PostTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "postTableViewCell", for: indexPath) as! PostTableViewCell
         
-        // 2
-        //cell.textLabel!.numberOfLines = 0
         cell.postTitleLabel.numberOfLines = 0
-        //cell.textLabel!.lineBreakMode = .byWordWrapping
         cell.postTitleLabel.lineBreakMode = .byWordWrapping
-        //cell.textLabel!.font = UIFont.systemFont(ofSize: 14.0)
         cell.postTitleLabel.font = UIFont.systemFont(ofSize: 14.0)
+        
         let post = self.posts[indexPath.row]
         cell.postTitleLabel.text = post.title
-  
+        
         return cell
     }
     
+    func retrieveAndParseData(){
+        let apiToContact = "https://www.reddit.com/.json"
+        Alamofire.request(apiToContact).validate().responseJSON() { response in
+            switch response.result{
+            case .success:
+                if let value = response.result.value {
+                    let json = JSON(value)
+                    let json_children = json["data"]["children"].array
+                    if let json_children = json_children{
+                        self.posts = json_children.flatMap({
+                            Post.init(json:$0)
+                        })
+                    }
+                }
+                
+            case .failure(let error):
+                print("API request did not succeed")
+                print(error)
+            }
+        }
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
